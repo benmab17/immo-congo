157e0cc5f5571a00ed7790d9d7e51f483f4850a3
Ben Mab <bienvenumabukama@gmail.com>
2026-03-16 21:42:29 +0100

    PWA: Ajout de la fonctionnalité d'installation sur mobile

--- immo_congo/urls.py
+++ immo_congo/urls.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles import finders
+from django.http import HttpResponse
 from django.urls import include, path
+
+
+def service_worker(request):
+    sw_path = finders.find("annonces/sw.js")
+    if not sw_path:
+        return HttpResponse("", content_type="application/javascript")
+    with open(sw_path, "r", encoding="utf-8") as sw_file:
+        response = HttpResponse(sw_file.read(), content_type="application/javascript")
+    response["Service-Worker-Allowed"] = "/"
+    return response
 
 urlpatterns = [
     path('', include('annonces.urls')),
     path('admin/', admin.site.urls),
+    path('sw.js', service_worker, name='service_worker'),
 ]
 
 if settings.DEBUG:
